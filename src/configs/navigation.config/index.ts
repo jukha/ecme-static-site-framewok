3e0e1e24568dfbf769f1bf31b94e4597111a9f9e
Junaid <junaid.apsc@gmail.com>
2025-06-18 05:16:31 +0500

    Refactor menu links structure

--- src/configs/navigation.config/index.ts
+++ src/configs/navigation.config/index.ts
@@ -10,8 +10,22 @@ import staticDocsNavigationConfig from './staticDocsNavigation' // Adjust path i
 
 const navigationConfig: NavigationTree[] = [
     // --- ADD OUR STATIC DOCS NAVIGATION HERE ---
-    ...staticDocsNavigationConfig,
-
+    {
+        key: 'concepts.landing',
+        path: '/home',
+        title: 'Home',
+        translateKey: 'nav.landing',
+        icon: 'homeicon',
+        type: NAV_ITEM_TYPE_ITEM,
+        authority: [],
+        meta: {
+            description: {
+                translateKey: 'nav.chatDesc',
+                label: 'Landing Page',
+            },
+        },
+        subMenu: [],
+    },
     {
         key: 'vaults',
         path: '',
@@ -60,7 +74,7 @@ const navigationConfig: NavigationTree[] = [
                 type: NAV_ITEM_TYPE_ITEM,
                 authority: [],
                 subMenu: [],
-                icon: '',
+                icon: 'networkicon',
             },
             {
                 key: 'monitors.stats',
@@ -70,7 +84,7 @@ const navigationConfig: NavigationTree[] = [
                 type: NAV_ITEM_TYPE_ITEM,
                 authority: [],
                 subMenu: [],
-                icon: '',
+                icon: 'statisticsicon',
             },
         ],
     },
@@ -116,37 +130,237 @@ const navigationConfig: NavigationTree[] = [
         ],
     },
     {
-        key: 'tools',
+        key: 'concepts.dao',
         path: '',
-        title: 'Tools',
-        translateKey: 'nav.tools',
-        icon: 'apiicon',
+        title: 'DAO',
+        translateKey: 'nav.conceptsDao.dao',
+        icon: 'daoicon',
         type: NAV_ITEM_TYPE_COLLAPSE,
         authority: [],
+        meta: {
+            description: {
+                translateKey: 'nav.conceptsDao.daoDesc',
+                label: 'Decentralized governance',
+            },
+        },
         subMenu: [
             {
-                key: 'tools.api',
-                path: '/tools/api',
-                title: 'API',
-                translateKey: 'nav.tools.api',
+                key: 'concepts.dao.structure',
+                path: '/dao/structure',
+                title: 'Structure',
+                translateKey: 'nav.conceptsDao.structure',
+                icon: 'structureicon',
                 type: NAV_ITEM_TYPE_ITEM,
                 authority: [],
+                meta: {
+                    description: {
+                        translateKey: 'nav.conceptsDao.structureDesc',
+                        label: 'Organization hierarchy',
+                    },
+                },
                 subMenu: [],
-                icon: '',
             },
             {
-                key: 'tools.sdk',
-                path: '/tools/sdk',
-                title: 'SDK',
-                translateKey: 'nav.tools.sdk',
+                key: 'concepts.dao.governance',
+                path: '/dao/governance',
+                title: 'Governance',
+                translateKey: 'nav.conceptsDao.governance',
+                icon: 'governanceicon',
                 type: NAV_ITEM_TYPE_ITEM,
                 authority: [],
+                meta: {
+                    description: {
+                        translateKey: 'nav.conceptsDao.governanceDesc',
+                        label: 'Proposals and voting',
+                    },
+                },
                 subMenu: [],
-                icon: '',
             },
         ],
     },
-    // ... any other route configurations imported or defined here
+
+    {
+        key: 'concepts.Legal',
+        path: '',
+        title: 'Legal',
+        translateKey: 'nav.conceptsLegal.Legal',
+        icon: 'law',
+        type: NAV_ITEM_TYPE_COLLAPSE,
+        authority: [],
+        meta: {
+            description: {
+                translateKey: 'nav.conceptsLegal.legalDesc',
+                label: 'Legal',
+            },
+        },
+        subMenu: [
+            {
+                key: 'concepts.legal.legal',
+                path: '/legal/legal-doc',
+                title: 'Legal Doc',
+                translateKey: 'nav.conceptsLegal.legal',
+                icon: 'legalicon',
+                type: NAV_ITEM_TYPE_ITEM,
+                authority: [],
+                meta: {
+                    description: {
+                        translateKey: 'nav.conceptsLegal.legal',
+                        label: 'Create new Legal Doc',
+                    },
+                },
+                subMenu: [],
+            },
+        ],
+    },
+    {
+        key: 'concepts.helpCenter',
+        path: '',
+        title: 'Help Center',
+        translateKey: 'nav.conceptsHelpCenter.helpCenter',
+        icon: 'helpCenter',
+        type: NAV_ITEM_TYPE_COLLAPSE,
+        authority: [],
+        meta: {
+            description: {
+                translateKey: 'nav.conceptsHelpCenter.helpCenterDesc',
+                label: 'Support and articles',
+            },
+        },
+        subMenu: [
+            {
+                key: 'concepts.helpCenter.tutorial',
+                path: '/help/tutorial/introduction',
+                title: 'Tutorial',
+                translateKey: 'nav.conceptsHelpCenter.tutorial',
+                icon: 'tutorialicon',
+                type: NAV_ITEM_TYPE_ITEM,
+                authority: [],
+                meta: {
+                    description: {
+                        translateKey: 'nav.conceptsHelpCenter.tutorial',
+                        label: 'Central support hub',
+                    },
+                },
+                subMenu: [],
+            },
+            {
+                key: 'concepts.helpCenter.userGuide',
+                path: '/help/user-guide',
+                title: 'User Guide',
+                translateKey: 'nav.conceptsHelpCenter.userGuide',
+                icon: 'userguideicon',
+                type: NAV_ITEM_TYPE_ITEM,
+                authority: [],
+                meta: {
+                    description: {
+                        translateKey: 'nav.conceptsHelpCenter.userGuide',
+                        label: 'Central support hub',
+                    },
+                },
+                subMenu: [],
+            },
+
+            {
+                key: 'concepts.helpCenter.FAQ',
+                path: '/help/faqs',
+                title: 'FAQ',
+                translateKey: 'nav.conceptsHelpCenter.FAQ',
+                icon: 'faqicon',
+                type: NAV_ITEM_TYPE_ITEM,
+                authority: [],
+                meta: {
+                    description: {
+                        translateKey: 'nav.conceptsHelpCenter.FAQ',
+                        label: 'Central support hub',
+                    },
+                },
+                subMenu: [],
+            },
+            {
+                key: 'concepts.helpCenter.examples',
+                path: '/help/examples',
+                title: 'Examples',
+                translateKey: 'nav.conceptsHelpCenter.examples',
+                icon: 'examplesicon',
+                type: NAV_ITEM_TYPE_ITEM,
+                authority: [],
+                meta: {
+                    description: {
+                        translateKey: 'nav.conceptsHelpCenter.examples',
+                        label: 'Central support hub',
+                    },
+                },
+                subMenu: [],
+            },
+            {
+                key: 'concepts.helpCenter.useCases',
+                path: '/help/use-cases',
+                title: 'Use Cases',
+                translateKey: 'nav.conceptsHelpCenter.useCases',
+                icon: 'usecasesicon',
+                type: NAV_ITEM_TYPE_ITEM,
+                authority: [],
+                meta: {
+                    description: {
+                        translateKey: 'nav.conceptsHelpCenter.useCases',
+                        label: 'Central support hub',
+                    },
+                },
+                subMenu: [],
+            },
+            {
+                key: 'concepts.helpCenter.zuz-dsl',
+                path: '/help/zuz-dsl/use-auth',
+                title: 'Zuz Dsl',
+                translateKey: 'nav.conceptsHelpCenter.zuz-dsl',
+                icon: 'zuzdslicon',
+                type: NAV_ITEM_TYPE_ITEM,
+                authority: [],
+                meta: {
+                    description: {
+                        translateKey: 'nav.conceptsHelpCenter.zuz-dsl',
+                        label: 'Central support hub',
+                    },
+                },
+                subMenu: [],
+            },
+        ],
+    },
+
+    {
+        key: 'concepts.about',
+        path: '',
+        title: 'About',
+        translateKey: 'nav.conceptsAbout.About',
+        icon: 'about',
+        type: NAV_ITEM_TYPE_COLLAPSE,
+        authority: [],
+        meta: {
+            description: {
+                translateKey: 'nav.conceptsAbout.aboutDesc',
+                label: 'About',
+            },
+        },
+        subMenu: [
+            {
+                key: 'concepts.about.about',
+                path: '/about',
+                title: 'About Doc',
+                translateKey: 'nav.conceptsAbout.about',
+                icon: 'abouticon',
+                type: NAV_ITEM_TYPE_ITEM,
+                authority: [],
+                meta: {
+                    description: {
+                        translateKey: 'nav.conceptsAbout.about',
+                        label: 'Create new About Docs',
+                    },
+                },
+                subMenu: [],
+            },
+        ],
+    },
+    ...staticDocsNavigationConfig,
 ]
 
 export default navigationConfig
